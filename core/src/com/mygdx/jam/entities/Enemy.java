deac56e855dd65c077c91dce2c179d7e4a3c1b0b
piotr-j <me@piotrjastrzebski.io>
2016-04-27 12:13:23 +0200

    enemy and player are damaged when touching

--- core/src/com/mygdx/jam/entities/Enemy.java
+++ core/src/com/mygdx/jam/entities/Enemy.java
@@ -123,7 +123,11 @@ public class Enemy extends Entity implements PhysicsObject {
 
     @Override
     public void handleBeginContact(PhysicsObject psycho2, GameWorld world) {
-
+        if (psycho2 instanceof Player) {
+            Player player = (Player)psycho2;
+            player.hp -= 0.1f;
+            health -= 20;
+        }
     }
 
     @Override
